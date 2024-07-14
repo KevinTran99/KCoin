e1b7853dab7cd7d11098a7aa0f70297def1f652c
KevinTran99 <kevintrann99@gmail.com>
2024-07-14 03:15:13 +0200

    Added PubNub integration to startup configuration

--- startup.mjs
+++ startup.mjs
@@ -1,3 +1,12 @@
 import Blockchain from './models/Blockchain.mjs';
+import PubNubServer from './pubnubServer.mjs';
+
+const credentials = {
+  publishKey: process.env.PUBLISH_KEY,
+  subscribeKey: process.env.SUBSCRIBE_KEY,
+  secretKey: process.env.SECRET_KEY,
+  userId: process.env.USER_ID,
+};
 
 export const blockchain = new Blockchain();
+export const pubnubServer = new PubNubServer({ blockchain, credentials });
