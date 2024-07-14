2782c1c71e1706b4cd036d047b983e863ea2a2c1
KevinTran99 <kevintrann99@gmail.com>
2024-07-14 03:07:54 +0200

    Removed unnecessary routes

--- routes/blockchain-routes.mjs
+++ routes/blockchain-routes.mjs
@@ -1,16 +1,12 @@
 import express from 'express';
 import {
-  broadcastBlock,
   mineBlock,
   getBlockchain,
-  synchronizeChain,
 } from '../controllers/blockchain-controller.mjs';
 
 const router = express.Router();
 
 router.route('/').get(getBlockchain);
 router.route('/mine').get(mineBlock);
-router.route('/consensus').get(synchronizeChain);
-router.route('/block/broadcast').post(broadcastBlock);
 
 export default router;
