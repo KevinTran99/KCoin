0ae4c73143372206007e9b88a8c0d165f431fbd1
KevinTran99 <kevintrann99@gmail.com>
2024-07-16 22:09:10 +0200

    Added elliptic curve cryptography support using secp256k1 (same as Bitcoin)

--- utilities/crypto-lib.mjs
+++ utilities/crypto-lib.mjs
@@ -1,5 +1,8 @@
 import crypto from 'crypto';
+import { ec } from 'elliptic';
 
 export const createHash = (stringToHash) => {
   return crypto.createHash('sha256').update(stringToHash).digest('hex');
 };
+
+export const ellipticHash = new ec('secp256k1');
