dc0a117ba2fa110e7509ac99ebd43c68c2f98724
KevinTran99 <kevintrann99@gmail.com>
2024-07-18 00:24:26 +0200

    Added transaction creation and error handling

--- controllers/transaction-controller.mjs
+++ controllers/transaction-controller.mjs
@@ -1,7 +1,19 @@
 import { transactionPool, wallet } from '../startup.mjs';
 
 export const addTransaction = (req, res, next) => {
-  res.status(201).json({ success: true, statusCode: 201, data: {} });
+  const { amount, recipient } = req.body;
+
+  let transaction;
+
+  try {
+    transaction = wallet.createTransaction({ recipient, amount });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, statusCode: 400, error: error.message });
+  }
+
+  res.status(201).json({ success: true, statusCode: 201, data: transaction });
 };
 
 export const getTransactionPool = (req, res, next) => {
