26108f4c7f437b9a72048b24e54f763d8b303870
KevinTran99 <kevintrann99@gmail.com>
2024-07-17 02:25:48 +0200

    Added Transaction class with input and output maps

---
+++ models/Transaction.mjs
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export default class Transaction {
+  constructor({ sender, recipient, amount }) {
+    this.id = uuidv4().replaceAll('-', '');
+    this.outputMap = this.createOutputMap({ sender, recipient, amount });
+    this.inputMap = this.createInputMap({ sender, outputMap: this.outputMap });
+  }
+
+  createOutputMap({ sender, recipient, amount }) {
+    const outputMap = {};
+
+    outputMap[recipient] = amount;
+    outputMap[sender.publicKey] = sender.balance - amount;
+
+    return outputMap;
+  }
+
+  createInputMap({ sender, outputMap }) {
+    return {
+      timestamp: Date.now(),
+      amount: sender.balance,
+      address: sender.publicKey,
+      signature: sender.sign(outputMap),
+    };
+  }
+}
