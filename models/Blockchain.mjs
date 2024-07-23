d9238b44ac9b647b85a6448484cc32d6a7b63800
KevinTran99 <kevintrann99@gmail.com>
2024-07-23 02:16:02 +0200

    Added transaction data validation to enforce mining reward and uniqueness rules

--- models/Blockchain.mjs
+++ models/Blockchain.mjs
@@ -1,5 +1,6 @@
 import { createHash } from '../utilities/crypto-lib.mjs';
 import Block from './Block.mjs';
+import Transaction from './Transaction.mjs';
 
 export default class Blockchain {
   constructor() {
@@ -82,6 +83,38 @@ export default class Blockchain {
       this.chain = chain;
     } else {
       return;
+    }
+  }
+
+  validateTransactionData({ chain }) {
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const transactionSet = new Set();
+      let counter = 0;
+
+      for (let transaction of block.data) {
+        if (transaction.inputMap.address === 'reward-adress') {
+          counter++;
+
+          if (counter > 1) return false;
+
+          if (
+            Object.values(transaction.outputMap)[0] !==
+            +process.env.MINING_REWARD
+          )
+            return false;
+        } else {
+          if (!Transaction.validate(transaction)) {
+            return false;
+          }
+
+          if (transactionSet.has(transaction)) {
+            return false;
+          } else {
+            transactionSet.add(transaction);
+          }
+        }
+      }
     }
   }
 
