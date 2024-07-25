822b053a8b464fed989cc196046f26faf3f2e283
KevinTran99 <kevintrann99@gmail.com>
2024-07-26 01:43:12 +0200

    Enhanced error handler to manage duplicate key and validation errors

--- middleware/errorHandler.mjs
+++ middleware/errorHandler.mjs
@@ -1,7 +1,21 @@
+import ErrorResponse from '../models/ErrorResponseModel.mjs';
+
 export const errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
+  let error = { ...err };
+
+  if (err.code === 11000) {
+    const message = 'Resource already exists';
+    error = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    statusCode: err.statusCode || 500,
-    error: err.message || 'Server Error',
+    statusCode: error.statusCode || 500,
+    error: error.message || 'Server Error',
   });
 };
