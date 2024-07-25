9499fc90c788d0c113d58af57bcf48cd2a9d6ae1
KevinTran99 <kevintrann99@gmail.com>
2024-07-25 23:03:50 +0200

    Integrated User model with register controller to handle user registration

--- controllers/auth-controller.mjs
+++ controllers/auth-controller.mjs
@@ -1,7 +1,11 @@
-export const register = (req, res, next) => {
-  res
-    .status(201)
-    .json({ success: true, statusCode: 201, message: 'register works' });
+import User from '../models/UserModel.mjs';
+
+export const register = async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({ name, email, password, role });
+
+  res.status(201).json({ success: true, statusCode: 201, message: user });
 };
 
 export const login = (req, res, next) => {
