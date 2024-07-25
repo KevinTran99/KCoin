dd208a997bc08c481ef26aa42d3c8aef3fb3bc46
KevinTran99 <kevintrann99@gmail.com>
2024-07-25 22:39:48 +0200

    Added Mongoose User schema with validation and defaults

---
+++ models/UserModel.mjs
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, 'Name required'] },
+  email: {
+    type: String,
+    required: [true, 'Email required'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Input a correct email',
+    ],
+  },
+  role: { type: String, enum: ['user', 'managar'], default: 'user' },
+  password: {
+    type: String,
+    required: [true, 'Password required'],
+    minlength: 5,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordTokenTokenExpire: Date,
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('User', userSchema);
