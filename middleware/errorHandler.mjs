239b3b08df905c7bae70e034b85187631c95c848
KevinTran99 <kevintrann99@gmail.com>
2024-07-28 01:04:35 +0200

    Ensure error message is properly copied in error handler

--- middleware/errorHandler.mjs
+++ middleware/errorHandler.mjs
@@ -3,6 +3,8 @@ import ErrorResponse from '../models/ErrorResponseModel.mjs';
 export const errorHandler = (err, req, res, next) => {
   let error = { ...err };
 
+  error.message = err.message;
+
   if (err.code === 11000) {
     const message = 'Resource already exists';
     error = new ErrorResponse(message, 400);
