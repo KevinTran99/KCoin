f85cf5226223fb8611850ffc2e6caf9cf6035c38
KevinTran99 <kevintrann99@gmail.com>
2024-07-31 20:57:02 +0200

    Added styling and rendering of user info for home page

--- KCoin-Client/src/pages/Home.jsx
+++ KCoin-Client/src/pages/Home.jsx
@@ -1,3 +1,27 @@
+import { useAuth } from '../context/AuthContext';
+import '../styles/home.css';
+
 export const Home = () => {
-  return <h1>Welcome to KCoin</h1>;
+  const { user } = useAuth();
+
+  return (
+    <>
+      {user ? (
+        <div className="home">
+          <h1>
+            Welcome back to KCoin <br />
+            {user.name}
+          </h1>
+
+          <h2>User info:</h2>
+          <ul>
+            <li>Role: {user.role}</li>
+            <li>Email: {user.email}</li>
+          </ul>
+        </div>
+      ) : (
+        <h1>Welcome to KCoin</h1>
+      )}
+    </>
+  );
 };
